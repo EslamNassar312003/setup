9e951d6dbd9cacfc6d707b50b1902ec133f25c03
EslamNassar312003 <47415802+EslamNassar312003@users.noreply.github.com>
2019-02-08 16:51:17 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,24 @@
+cd
+pkg install git -y
+pkg install toilet -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install nano -y
+pkg install unzip -y
+
+
+
+
+
+git clone https://github.com/EslamNassar312003/nassar
+
+cd nassar
+
+unzip nassar1.zip
+unzip nassar2.zip
+unzip nassar3.zip
+unzip nassar0.zip
+cp nassar.sh /data/data/com.termux/files/usr/bin/nassar
+chmod +x /data/data/com.termux/files/usr/bin/nassar
+bash nassar.sh
